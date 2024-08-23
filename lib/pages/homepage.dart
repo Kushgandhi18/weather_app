c3cea852f1be7ed6899398f9770d3b416a9170be
Kush gandhi <127643258+Kushgandhi18@users.noreply.github.com>
2024-08-23 23:59:33 +0530

    initial commit

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final _weatherService= WeatherService('e2bdc1657a973f97572b2026bed9d4ea');
+  Weather? _weather;
+
+  _fetchWeather() async{
+    String cityName = await _weatherService.getCurrentCity();
+
+    try{
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather= weather;
+      });
+    }
+    catch(e){
+      print(e);
+    }
+  }
+
+  String getWeatherAnimation(String? mainCondtion ){
+
+    if(mainCondtion == null) return 'assets/sunny.json';
+
+    switch(mainCondtion.toLowerCase()){
+      case 'cloud':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+
+    }
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName??"loading city..."),
+
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
+            Text('${_weather?.temperature.round()}°C'),
+
+            Text(_weather?.mainCondition??""),
+
+
+          ],
+        ),
+      ),
+
+
+    ));
+  }
+}
